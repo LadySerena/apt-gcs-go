3532c11c45ba1531b71627733137a0be2b65a820
LadySerena <serena.tiede+github@gmail.com>
2021-04-04 20:23:53 -0500

    feat: add apt message encoding method
    this will handle converting the structs to the plaintext message

--- go.mod
+++ go.mod
@@ -1,3 +1,18 @@
 module github.com/LadySerena/apt-gcs-go
 
 go 1.16
+
+require (
+	cloud.google.com/go v0.81.0 // indirect
+	cloud.google.com/go/storage v1.14.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	go.opentelemetry.io/otel v0.16.0 // indirect
+	go.opentelemetry.io/otel/exporters/stdout v0.16.0 // indirect
+	go.opentelemetry.io/otel/sdk v0.16.0 // indirect
+	golang.org/x/mod v0.4.2 // indirect
+	golang.org/x/net v0.0.0-20210331212208-0fccb6fa2b5c // indirect
+	golang.org/x/oauth2 v0.0.0-20210402161424-2e8d93401602 // indirect
+	golang.org/x/sys v0.0.0-20210403161142-5e06dd20ab57 // indirect
+	golang.org/x/text v0.3.6 // indirect
+)
--- pkg/apt/transport.go
+++ pkg/apt/transport.go
@@ -1,5 +1,14 @@
 package apt
 
+import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+
+	"cloud.google.com/go/storage"
+)
+
 //basic flow
 //init gcs client on start
 //send out 100 capabilities message to apt on startup
@@ -8,3 +17,76 @@ package apt
 //get the thing from gcs
 //send 201 if happy or 400 if sad
 //there's something weird with the encoding per https://github.com/dhaivat/apt-gcs/issues/1
+
+var (
+	codeMap = map[int]string{
+		100: "Capabilities",
+		101: "Log",
+		102: "Status",
+		200: "URI Start",
+		201: "URI Done",
+		400: "URI Failure",
+		600: "URI Acquire",
+		601: "Configuration",
+	}
+)
+
+type GCSTransport struct {
+	client     *storage.Client
+	bucket     *storage.BucketHandle
+	bucketName string
+}
+
+type Message struct {
+	Code int
+	Content map[string]string
+}
+
+
+func NewGcsTransport(ctx context.Context, projectID string, bucketName string) (*GCSTransport, error) {
+	client, clientCreateErr := storage.NewClient(ctx)
+	if clientCreateErr != nil {
+		return nil, clientCreateErr
+	}
+	bucketHandle := client.Bucket(bucketName)
+
+	return &GCSTransport{
+		client:     client,
+		bucket:     bucketHandle,
+		bucketName: bucketName,
+	}, nil
+}
+
+func (t GCSTransport) Close() error {
+	return t.client.Close()
+}
+
+// encode takes a plaintext message struct and generates a proper plaintext string for example
+// $codeNumber: $debugText\n
+// $fieldName: $fieldValue\n
+// \n
+func (m Message) encode() (string, error) {
+	var messageBuffer bytes.Buffer
+	codeHeader := fmt.Sprintf("%d: %s\n", m.Code, codeMap[m.Code])
+	bytesWritten, writeErr := messageBuffer.WriteString(codeHeader)
+	if writeErr != nil {
+		return "", writeErr
+	}
+	if bytesWritten != len(codeHeader) {
+		return "", errors.New("bytes written does not match header")
+	}
+	for key, value := range m.Content {
+		if key != "" && value != "" {
+			content := fmt.Sprintf("%s: %s\n", key, value)
+			lengthWritten, contentWriteErr := messageBuffer.WriteString(content)
+			if contentWriteErr != nil {
+				return "", contentWriteErr
+			}
+			if lengthWritten != len(content){
+				return "", errors.New("write length mismatch")
+			}
+		}
+	}
+	messageBuffer.WriteRune('\n')
+	return messageBuffer.String(), nil
+}
